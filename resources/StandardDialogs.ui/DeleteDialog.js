498d05686daeed79f0f85adf577648645157f0ef
rvogel <vogel@hallowelt.biz>
2023-03-23 21:30:33 +0100

    Add "delete subpages" functionality
    ERM32073
    
    [4.x]
    
    Change-Id: I870c94c8be0a6f2bf5a6b47b7c2eebe898f0eb6f

--- resources/StandardDialogs.ui/DeleteDialog.js
+++ resources/StandardDialogs.ui/DeleteDialog.js
@@ -28,6 +28,11 @@ StandardDialogs.ui.DeleteDialog.prototype.getFormItems = function () {
 		value: 'watch',
 		selected: true
 	} );
+	this.deleteSubpagesCheckbox = new OO.ui.CheckboxInputWidget( {
+		id: this.elementId + '-cb-delete-subpages',
+		value: 'delete-subpages',
+		selected: true
+	} );
 
 	return [
 		new OO.ui.FieldsetLayout( {
@@ -43,6 +48,10 @@ StandardDialogs.ui.DeleteDialog.prototype.getFormItems = function () {
 				new OO.ui.FieldLayout( this.watchCheckbox, {
 					label: mw.message( 'watchthis' ).plain(),
 					align: 'inline'
+				} ),
+				new OO.ui.FieldLayout( this.deleteSubpagesCheckbox, {
+					label: mw.message( 'standarddialogs-delete-subpages' ).plain(),
+					align: 'inline'
 				} )
 			]
 		} )
@@ -55,24 +64,123 @@ StandardDialogs.ui.DeleteDialog.prototype.makeDoneActionProcess = function () {
 	const dfd = new $.Deferred();
 	mw.loader.using( 'mediawiki.api' ).done( function () {
 		const mwApi = new mw.Api();
-		const params = {
-			action: 'delete',
-			title: dialog.pageName,
-			reason: dialog.reasonCombo.getValue() + dialog.otherReasonText.getValue()
-		};
-		if ( dialog.watchCheckbox.isSelected() ) {
-			params.watchlist = 'watch';
-		} else {
-			params.watchlist = 'unwatch';
-		}
-		mwApi.postWithToken( 'csrf', params ).done( function ( data ) {
-			dfd.resolve.apply( dialog, arguments );
-		} ).fail( function () {
-			dfd.reject.apply( dialog, [ new OO.ui.Error( arguments[ 0 ], { recoverable: false } ) ] );
+		const getPagesToDeletePromise = dialog.getPagesToDelete( dialog.pageName, mwApi );
+		getPagesToDeletePromise.done( function ( pages ) {
+			if ( pages.length > 10 ) {
+				dialog.confirmMassDeletion( pages.length ).done( function () {
+					dialog.performDeletion( dfd, pages );
+				} )
+					.fail( function () {
+						dfd.reject.apply( dialog, arguments );
+					} );
+			} else {
+				dialog.performDeletion( dfd, pages );
+			}
 		} );
 	} ).fail( function () {
 		dfd.reject.apply( dialog, arguments );
 	} );
 
 	return new OO.ui.Process( dfd.promise(), this );
+};
+
+StandardDialogs.ui.DeleteDialog.prototype.confirmMassDeletion = function ( numberOfPages ) {
+	const dfd = new $.Deferred();
+	const confirmMessage = mw.message(
+		'standarddialogs-confirm-mass-deletion', numberOfPages ).plain();
+	OO.ui.confirm( confirmMessage ).done( function ( confirmed ) {
+		if ( confirmed ) {
+			dfd.resolve();
+		} else {
+			dfd.reject();
+		}
+	} );
+	return dfd.promise();
+};
+
+StandardDialogs.ui.DeleteDialog.prototype.performDeletion = function ( dfd, pages ) {
+	const deletePageApiCallPromises = [];
+	const params = {
+		action: 'delete',
+		reason: this.reasonCombo.getValue() + this.otherReasonText.getValue()
+	};
+	if ( dialog.watchCheckbox.isSelected() ) {
+		params.watchlist = 'watch';
+	} else {
+		params.watchlist = 'unwatch';
+	}
+	for ( let i = 0; i < pages.length; i++ ) {
+		params.title = pages[ i ];
+		const deletePageApiCallPromise = mwApi.postWithToken(
+			'csrf',
+			// pass a copy of params to avoid overwriting the title
+			$.extend( {}, params )
+		);
+		deletePageApiCallPromises.push( deletePageApiCallPromise );
+	}
+	$.when.apply( $, deletePageApiCallPromises ).done( function ( data ) {
+		dfd.resolve.apply( this, arguments );
+	} ).fail( function () {
+		const error = new OO.ui.Error( arguments[ 0 ], { recoverable: false } );
+		dfd.reject.apply( this, [ error ] );
+	} );
+};
+
+StandardDialogs.ui.DeleteDialog.prototype.getPagesToDelete = function ( pageName, mwApi ) {
+	const dfd = new $.Deferred();
+	const title = mw.Title.newFromText( pageName );
+	let pagesToDelete = [ pageName ];
+	if ( this.deleteSubpagesCheckbox.isSelected() ) {
+		const getSubpagesPromise = this.getSubpages( title, mwApi );
+		getSubpagesPromise.done( function ( subpages ) {
+			pagesToDelete = pagesToDelete.concat( subpages );
+			dfd.resolve( pagesToDelete );
+		} ).fail( function () {
+			dfd.reject.apply( this, arguments );
+		} );
+	} else {
+		dfd.resolve( pagesToDelete );
+	}
+
+	return dfd.promise();
+};
+
+StandardDialogs.ui.DeleteDialog.prototype.getSubpages = function ( title, mwApi, contd ) {
+	const dialog = this;
+	const dfd = new $.Deferred();
+	const params = {
+		action: 'query',
+		list: 'allpages',
+		apnamespace: title.getNamespaceId(),
+		apfilterredir: 'nonredirects',
+		apprefix: title.getMainText() + '/',
+		apdir: 'ascending',
+		aplimit: 'max',
+		approp: 'info'
+	};
+	if ( contd ) {
+		params.apcontinue = contd;
+	}
+	mwApi.get( params ).done( function ( data ) {
+		const subpages = [];
+		if ( data.query.allpages ) {
+			for ( let i = 0; i < data.query.allpages.length; i++ ) {
+				subpages.push( data.query.allpages[ i ].title );
+			}
+		}
+		if ( data.continue ) {
+			const getSubpagesPromise = dialog.getSubpages( title, mwApi, data.continue.apcontinue );
+			getSubpagesPromise.done( function ( subpages2 ) {
+				dfd.resolve( subpages.concat( subpages2 ) );
+			} ).fail( function () {
+				dfd.reject.apply( dialog, arguments );
+			} );
+		} else {
+			dfd.resolve( subpages );
+		}
+	} ).fail( function () {
+		dfd.reject.apply( dialog, arguments );
+	} );
+
+	return dfd.promise();
 };
