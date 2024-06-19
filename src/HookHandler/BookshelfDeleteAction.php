bcbdb97677577e4a572e5e3049195638a6d6d814
Mschlindwein <schlindwein@hallowelt.com>
2024-06-19 11:35:09 +0200

    Add integration for delete book
    ERM37685
    
    [4.5.x]
    
    Change-Id: Id4fde6a49996f43fb8f29363b27294ef0332ef65

---
+++ src/HookHandler/BookshelfDeleteAction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MediaWiki\Extension\StandardDialogs\HookHandler;
+
+use BlueSpice\Bookshelf\Hook\BSBookshelfBooksOverviewBeforeSetBookActions;
+use MediaWiki\Extension\StandardDialogs\BookOverviewActions\BookOverviewDelete;
+use Title;
+
+class BookshelfDeleteAction implements BSBookshelfBooksOverviewBeforeSetBookActions {
+
+	public function onBSBookshelfBooksOverviewBeforeSetBookActions(
+		array &$actions,
+		Title $book,
+		string $displayTitle
+	): void {
+		if ( !isset( $actions['delete'] ) ) {
+			return;
+		}
+
+		$actions[ 'delete' ] = new BookOverviewDelete( $book, $displayTitle );
+	}
+}
