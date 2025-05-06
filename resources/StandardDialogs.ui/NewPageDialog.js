f6c29f26a2c3e1d99238384547e21750d1a01c65
d.savuljesku <d.savuljesku@gmail.com>
2025-05-06 13:34:38 +0200

    Show error when trying to create page with invalid chars
    [5.x]
    
    ERM41437
    
    Change-Id: I10e4c34e578c51df68b6fdd415233bf85a967c0a
    (cherry picked from commit 36ce27a1fe477617ff27fdac9e03d565c7803c4d)

--- resources/StandardDialogs.ui/NewPageDialog.js
+++ resources/StandardDialogs.ui/NewPageDialog.js
@@ -27,17 +27,21 @@ StandardDialogs.ui.NewPageDialog.prototype.getFormItems = function () {
 		mustExist: false,
 		contentPagesOnly: false
 	} );
+	this.mainInput.connect( this, {
+		change: 'onTitleChange'
+	} );
+	this.mainFieldset = new OO.ui.FieldLayout( this.targetTitle, {
+		label: mw.message( 'standarddialogs-new-page-label' ).plain(),
+		align: 'top'
+	} );
 
-	return [
-		new OO.ui.FieldsetLayout( {
-			items: [
-				new OO.ui.FieldLayout( this.targetTitle, {
-					label: mw.message( 'standarddialogs-new-page-label' ).plain(),
-					align: 'top'
-				} )
-			]
-		} )
-	];
+	return [ this.mainFieldset ];
+};
+
+StandardDialogs.ui.NewPageDialog.prototype.getSetupProcess = function ( data ) {
+	return StandardDialogs.ui.NewPageDialog.super.prototype.getSetupProcess.call( this, data ).next( () => {
+		this.actions.setAbilities( { done: false } );
+	} );
 };
 
 StandardDialogs.ui.NewPageDialog.prototype.makeDoneActionProcess = function () {
@@ -46,4 +50,52 @@ StandardDialogs.ui.NewPageDialog.prototype.makeDoneActionProcess = function () {
 };
 StandardDialogs.ui.NewPageDialog.prototype.getActionCompletedEventArgs = function ( action ) {
 	return [ this.newTitle ];
+};
+
+StandardDialogs.ui.NewPageDialog.prototype.onTitleChange = function ( value ) {
+	if ( this.typeTimeout ) {
+		clearTimeout( this.typeTimeout );
+	}
+	this.typeTimeout = setTimeout( () => {
+		this.checkValidity( value );
+	}, 500 );
+};
+
+StandardDialogs.ui.NewPageDialog.prototype.checkValidity = function ( value ) {
+	if ( !value ) {
+		this.actions.setAbilities( { done: false } );
+		return;
+	}
+	new mw.Api().get( {
+		action: 'query',
+		prop: 'pageprops',
+		titles: value
+	} ).done( ( data ) => {
+		// Check if there is data.query.pages.-1
+		if ( data.query && data.query.pages && data.query.pages[ -1 ] ) {
+			// eslint-disable-next-line no-prototype-builtins
+			if ( data.query.pages[ -1 ].hasOwnProperty( 'invalid' ) ) {
+				this.actions.setAbilities( { done: false } );
+				this.setError( data.query.pages[ -1 ].invalidreason );
+			} else {
+				this.clearError();
+				this.actions.setAbilities( { done: true } );
+			}
+		} else {
+			this.actions.setAbilities( { done: false } );
+		}
+	} ).fail( () => {
+		// Something went wrong, let user go to the page and deal with it there
+		this.actions.setAbilities( { done: true } );
+	} );
+};
+
+StandardDialogs.ui.NewPageDialog.prototype.setError = function ( error ) {
+	this.mainFieldset.setErrors( [ error ] );
+	this.mainInput.lookupMenu.toggle( false );
+	this.updateSize();
+};
+
+StandardDialogs.ui.NewPageDialog.prototype.clearError = function () {
+	this.mainFieldset.setErrors( [] );
 };
