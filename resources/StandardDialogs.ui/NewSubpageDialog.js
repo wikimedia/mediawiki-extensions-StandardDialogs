77328a1c52455bcd69b1b2fd373aa0a7cdabf467
Jonas Hillebrand <jonas.hillebrand@hallowelt.com>
2026-08-27 15:39:08 +0200

    Require a name in the new subpage dialog
    ERM49900
    
    [5.x][galaxy]
    
    Related to: https://github.com/BlueSpice-Wiki/mediawiki-extensions-Minutes/pull/4
    Change-Id: Ifafe14b99db05870d1be33b3bf92874dc474c8d0
    (cherry picked from commit ec9c3bea0e827209c43ed562c793457d252de0ec)

--- resources/StandardDialogs.ui/NewSubpageDialog.js
+++ resources/StandardDialogs.ui/NewSubpageDialog.js
@@ -15,6 +15,19 @@ StandardDialogs.ui.NewSubpageDialog.prototype.makeSetupProcessData = function ()
 	return data;
 };
 
+/**
+ * The name is required, so the dialog opens with an empty field and nothing to create yet.
+ *
+ * @param {Object} data
+ * @return {OO.ui.Process}
+ */
+StandardDialogs.ui.NewSubpageDialog.prototype.getSetupProcess = function ( data ) {
+	return StandardDialogs.ui.NewSubpageDialog.super.prototype.getSetupProcess.call( this, data )
+		.next( () => {
+			this.actions.setAbilities( { done: false } );
+		}, this );
+};
+
 StandardDialogs.ui.NewSubpageDialog.prototype.getPrimaryActionLabel = function () {
 	return mw.message( 'standarddialogs-new-subpage-btn-label' ).plain();
 };
@@ -25,7 +38,8 @@ StandardDialogs.ui.NewSubpageDialog.prototype.getFormItems = function () {
 		$overlay: this.$overlay,
 		mustExist: false,
 		prefix: this.pageName + '/',
-		contentPagesOnly: false
+		contentPagesOnly: false,
+		required: true
 	} );
 	this.mainInput.connect( this, {
 		change: 'onTitleChange'
@@ -55,6 +69,12 @@ StandardDialogs.ui.NewSubpageDialog.prototype.getActionCompletedEventArgs = func
 StandardDialogs.ui.NewSubpageDialog.prototype.onTitleChange = function ( value ) {
 	if ( this.typeTimeout ) {
 		clearTimeout( this.typeTimeout );
+	}
+	// Whatever was checked before does not hold for the new value.
+	this.actions.setAbilities( { done: false } );
+	if ( !value.trim() ) {
+		this.clearError();
+		return;
 	}
 	this.typeTimeout = setTimeout( () => {
 		this.validateTitleNotExist( this.pageName + '/' + value );
